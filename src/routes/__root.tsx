0801a2808eeb7a64e086dc6c86023951b09afbad
matellordson <snr.mn.lordson@gmail.com>
2025-11-10 14:49:38 +0100

    feat: add routing for product and root components with TanStack Router

---
+++ src/routes/__root.tsx
@@ -0,0 +1,17 @@
+import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
+
+const RootLayout = () => (
+  <>
+    <div className="p-2 flex gap-2">
+      <Link to="/product" className="[&.active]:font-bold">
+        Product
+      </Link>
+    </div>
+    <hr />
+    <Outlet />
+    <TanStackRouterDevtools />
+  </>
+);
+
+export const Route = createRootRoute({ component: RootLayout });
